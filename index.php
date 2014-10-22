b33885513397ab30bbb4a5f116ff66dd96ef1945
OaSiis <maxcharbonnelle@gmail.com>
2014-10-22 13:01:04 +0200

    Update slugify

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+]);
+
+$twig = new Twig_Environment($loader, [
+    //'cache' => null,
+]);
+
+$article =[
+    'name' => 'azeaz',
+    'content' => 'zadzdazfdaf'
+];
+
+echo $twig->render('article.html.twig', [
+    'article' => $article,
+]);
